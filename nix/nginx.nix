519873486162ec21c2f1c206e3c5c0d581d23584
Macario Ortega <git@aelita.io>
2026-01-15 18:18:57 +0100

    Hot reload css

--- nix/nginx.nix
+++ nix/nginx.nix
@@ -29,8 +29,15 @@ let
         server_name localhost;
 
         location / {
-          alias SUBMISSIONS_PATH/;
-          try_files $uri $uri/ /index.html;
+          proxy_pass http://localhost:8000;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_read_timeout 86400;
         }
 
         location /back-office/ {
